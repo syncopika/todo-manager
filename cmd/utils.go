683dcc40f176f4d3dfa3d7c0a4c51541cb906f0a
nicholas hung <nhung93@outlook.com>
2021-09-07 23:20:41 -0400

    trying out survey now instead of promptui

--- cmd/utils.go
+++ cmd/utils.go
@@ -4,31 +4,51 @@ import (
 	"io/ioutil"
 	"fmt"
 
-	"github.com/manifoldco/promptui"
+	"github.com/AlecAivazis/survey/v2"
+	//"github.com/manifoldco/promptui"
 )
+
 
 // let the user find the TODO file they want to list the tasks of
 func SelectFile(dirPath string) string {
 	// get all the files in todo-lists/
 	files, err := ioutil.ReadDir(dirPath) // []fs.FileInfo is returned
+	if err != nil {
+		fmt.Println("error reading todo list directory!")
+		return ""
+	}
 	
 	var fileNames []string
 	for _, file := range files {
 		fileNames = append(fileNames, file.Name())
 	}
 	
-	prompt := promptui.Select{
-		Label: "Select the TODO list",
-		Items: fileNames,
+	var answer = struct {
+		Select string
+	}{}
+	
+	
+	qs := []*survey.Question{
+		{
+			Name: "select",
+			Prompt: &survey.Select{
+				Message: "Select the TODO list",
+				Options: fileNames,
+				Default: "todo.txt",
+			},
+		},
 	}
 	
-	_, fileToSelect, err := prompt.Run()
+	err2 := survey.Ask(qs, &answer)
 	
-	if err != nil {
+	
+	if err2 != nil {
+		fmt.Println(err2.Error())
 		fmt.Println("ruh roh, TODO file selection failed!")
 		return "" // should rather return error?
 	}else{
 		// return selected filepath
+		fileToSelect := answer.Select
 		return fmt.Sprintf("todo-lists/%s", fileToSelect)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/syncopika/todo-manager
 go 1.15
 
 require (
+	github.com/AlecAivazis/survey/v2 v2.3.1
 	github.com/manifoldco/promptui v0.8.0
 	github.com/spf13/cobra v1.2.1
 	github.com/spf13/viper v1.8.1
